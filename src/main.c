9996d6cbefda1a151163cd58e35a086c25769333
Felix Trinh <felixtrinh@Air-von-Felix-2.fritz.box>
2026-08-26 17:39:55 +0200

    Treiber und co initialisiert

--- src/main.c
+++ src/main.c
@@ -2,21 +2,43 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
+#include "driver/i2c.h"
+#include "esp_log.h"
 
-#define LED_PIN GPIO_NUM_2
+static const char *TAG = "VIBRO";
 
-void app_main(void)
+
+#define I2C_MASTER_SCL_IO 22
+#define I2C_MASTER_SDA_IO 21
+#define I2C_MASTER_NUM I2C_NUM_0
+#define I2C_MASTER_FREQ_HZ 400000
+// MPU6050 device address
+#define MPU6050_ADDR 0x68
+#define MPU6050_PWR_MGMT_1 0x6B
+#define MPU6050_ACCEL_XOUT_H 0x3B
+#define MPU6050_ACCEL_YOUT_H 0x3D
+#define MPU6050_ACCEL_ZOUT_H 0x3F
+#define WHO_AM_I_REG 0x75
+#define WHO_AM_I_EXPECTED 0x68
+#define CONFIG_REG 0x1A
+
+// initialize I2C master
+static esp_err_t i2c_master_init(void) 
 {
-    gpio_reset_pin(LED_PIN);
-    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .scl_io_num = I2C_MASTER_SCL_IO,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = I2C_MASTER_FREQ_HZ,
+    };
+    return i2c_param_config(I2C_MASTER_NUM, &conf);
 
-    while (1) {
-        gpio_set_level(LED_PIN, 1);
-        printf("LED an\n");
-        vTaskDelay(pdMS_TO_TICKS(500));
-
-        gpio_set_level(LED_PIN, 0);
-        printf("LED aus\n");
-        vTaskDelay(pdMS_TO_TICKS(500));
-    }
+}
+// Write register to MPU6050
+static esp_err_t mpu6050_write_reg(uint8_t reg, uint8_t data) 
+{
+    uint8_t write_buf[2] = {reg, data};
+    return i2c_master_write_to_device(I2C_MASTER_NUM, MPU6050_ADDR, write_buf, sizeof(write_buf), pdMS_TO_TICKS(1000));
 }
